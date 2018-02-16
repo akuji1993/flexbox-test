6d1a52fa2997eea5299d7de0654e7fa5bfd5b1cf
Tobias Arweiler <t.arweiler@nutmountain.io>
2018-02-16 16:14:09 +0100

    added react-router-dom and split some css code

---
+++ src/js/pages/Animation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Animation extends React.Component {
+
+  render() {
+    return (
+      <div>Animation</div>
+    );
+  }
+}
+
+export default Animation;
---
+++ src/js/pages/index.js
@@ -0,0 +1,4 @@
+export {default as Flexbox} from './Flexbox';
+export {default as Animation} from './Animation';
+export {default as D3Component} from './D3Component';
+export {default as WorldDomination} from './WorldDomination';
